0e5aab41e3295d12af522073b85aac8d6765cb0a
sajjadhesami <34142871+sajjadhesami@users.noreply.github.com>
2022-04-13 23:54:41 +0430

    Finished

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # SelfAvoidingRandomWalk
-This is my implementation of Self-avoiding random walk in Python using Monte-Carlo method. It receives two arguments (1) the size of the lattice and (2) the number of trials from the user as system arguments and visualizes the process on the screen. 
+This is my implementation of Self-avoiding random walk in Python using Monte-Carlo method. It receives three arguments from the user (1) the size of the lattice, (2) the number of trials, and (3) a zero or one value to determine whether it should visualize the process on the screen or not.
+It prints the movements of the random walker on the console.
+
+The implementation uses "color.py" and "stddraw.py" from [here](https://introcs.cs.princeton.edu/python/code/).
 
 A description of this project can be found [here](https://en.wikipedia.org/wiki/Self-avoiding_walk) and [here](https://www.youtube.com/watch?v=nLaw8ageZnA) (a lecture by Prof. Seyed Naser Razavi at the University of Tabriz. The lecture is in Persian).
--- SelfAvoidingRandomWalk.py
+++ SelfAvoidingRandomWalk.py
@@ -1,26 +1,111 @@
-import sys,random,stddraw,color
-
+import sys, random, stddraw, color
+import tkinter
 
 def drawMap(n):
-    stddraw.setXscale(0,n)
-    stddraw.setYscale(0,n)
+    '''
+    This function receives the size of the map and draws a grid on the canvas
+    :param n: size of the map
+    :return: NoneType (internally draws the canvas)
+    '''
+    stddraw.clear(color.WHITE)
+    stddraw.setPenColor(color.BLACK)
+    stddraw.setXscale(0, n)
+    stddraw.setYscale(0, n)
 
-    for i in range(0,n):
-            stddraw.line(i,0,i,n)
-            stddraw.line(0,i,n,i)
-if __name__=="__main__":
-    n=int(sys.argv[1])
-    lattice=[[False]*n]*n
-    drawMap(n)
+    for i in range(0, n):
+        stddraw.line(i, 0, i, n)
+        stddraw.line(0, i, n, i)
+
+
+def drawCircle(x, y, lattice,path):
+    '''
+    Moves the red circle from its previous place to the new place
+    :param x: new X of the circle
+    :param y: new Y of the circle
+    :param lattice: lattice
+    :param path: path variable
+    :return: NoneType
+    '''
+    N=len(lattice)
+    drawMap(N)
+
+
+    stddraw.setPenColor(color.BLACK)
+    stddraw.setPenRadius(0.05)
+
+    for i in range(0,len(path)-1):
+        stddraw.line(path[i][0],N-path[i][1],path[i+1][0],N-path[i+1][1])
+
+
+    stddraw.setPenRadius()
     stddraw.setPenColor(color.RED)
+    stddraw.filledCircle(x, N-y, 0.25)
 
-    x=n/2
-    y=n/2
-    lattice[x][y]=True
-    stddraw.filledCircle(x,y,0.5)
+def main():
+    if (len(sys.argv) != 4):
+        print("please run the script with three arguments: SlefAvoidingRandomWalk.py 10 100 1")
+        return
+    N = int(sys.argv[1])
+    T = int(sys.argv[2])
+    SHOW = bool(int(sys.argv[3]))
 
-    while(x-1>0 and x+1<n and y-1>0 and y+1<n and (lattice[x-1][y]!=False or lattice[x+1][y]!=False or lattice[x][y-1]!=False or lattice[x][y+1])):
+    escape_counter = 0
+    deadend_counter = 0
+
+    for t in range(0, T):
+        lattice = [[False] * N for i in range(N)]
+        x = N // 2
+        y = N // 2
+        lattice[x][y] = True
+
+        if(SHOW==True):
+            path=[(x,y)]
+            drawCircle(x,y,lattice,path)
+            stddraw.show(200)
+
+        while True:
+
+            print("move from (%-10d,%-10d)" %(x,y),end=" ")
+            while (True):
+                r = random.randrange(0, 4)
+                if r == 0:
+                    if (lattice[x + 1][y]):
+                        continue
+                    x += 1
+                    break
+                elif r == 1:
+                    if (lattice[x - 1][y]):
+                        continue
+                    x -= 1
+                    break
+                elif r == 2:
+                    if (lattice[x][y + 1]):
+                        continue
+                    y += 1
+                    break
+                else:
+                    if (lattice[x][y - 1]):
+                        continue
+                    y -= 1
+                    break
+            print("(%-10d,%-10d)" %(x,y))
+            lattice[x][y] = True
+            if(SHOW==True):
+                path.append((x,y))
+                drawCircle(x,y,lattice,path)
+                stddraw.show(200)
+            if (x - 1 < 0 or x + 1 >= N or y - 1 < 0 or y + 1 >= N):
+                escape_counter += 1
+                break
+            if (lattice[x - 1][y] == True and lattice[x + 1][y] == True and lattice[x][y - 1] == True and lattice[x][
+                y + 1] == True):
+                deadend_counter += 1
+                break
+
+        print("Experiment #%-10d has just finished: #escape: %-10d #deadend: %-10d" % (
+        t+1, escape_counter, deadend_counter))
 
 
-        stddraw.show(100)
 
+if __name__ == "__main__":
+    main()
